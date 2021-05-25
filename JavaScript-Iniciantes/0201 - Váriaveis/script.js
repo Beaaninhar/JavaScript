1f59e4a8f7c3ddccd27e09ab1fefaa1b7f41561e
Ana Beatriz <ana.beatriz@eurekalabs.com.br>
2021-05-25 10:59:02 -0300

    Aulas 0101 até 0202

---
+++ JavaScript-Iniciantes/0201 - Váriaveis/script.js
@@ -0,0 +1,77 @@
+var nome = "Ana";
+var  idade = 21;
+var fazFaculdade = true;
+
+console.log(`Olá! Me chamo ${nome} tenho ${idade} anos` )
+
+var preco = 25;
+var totalCompra = 5;
+var totalPreço = preco * totalCompra;
+
+console.log(`Valor total da compra: R${totalPreço}`)
+
+
+var sobrenome = 'Beatriz',
+      cidade = 'Maringá';
+
+console.log(`Meu sobrenome é ${sobrenome} e venho de ${Maringá}`);
+
+var semValor;
+
+console.log(`Sem definir: ${semValor}`)
+
+
+// Inválido
+// var §ana;
+// var function;
+// var 1idade;
+
+// Válido
+var $selecionar;
+var _nome;
+var possuiFaculdadeNoExterior;
+
+
+console.log(nomeSobrenome);
+var nomeSobrenome = 'Ana Beatriz';
+
+var profissao = 'Programadora';
+console.log(profissao);
+
+
+var time = 'São Paulo';
+time = 'Curitiba';
+
+let precoAtualizado = 90;
+precoAtualizado = 95;
+
+const possuiEnsinoMedio = true;
+possuiEnsinoMedio = false;
+// Retorna um erro
+
+
+// Exercícios
+// Declarar uma variável com o seu nome
+  var meuNome = 'Ana Beatriz';
+
+// Declarar uma variável com a sua idade
+
+let minhaIdade = 21;
+
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+let comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Batata Frita'
+
+// Declarar 5 variáveis diferentes sem valores
+
+let filmeFavorito;
+let jogoFavorito,
+    oQueAlmocar,
+    cafeDaTarde,
+    oQueFazerAmanha;
+
---
+++ JavaScript-Iniciantes/0202 - Tipos-de-dados/script.js
@@ -0,0 +1,59 @@
+// var nome = 'Ana"';
+// var idade = 21;
+// var time = null;
+// var simbol = Symbol();
+
+// console.log(typeof time);
+// console.log(typeof simbol);
+
+
+var nome = "Ana";
+var sobrenome = "Beatriz";
+var nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
+
+
+var gols = 1000;
+var frase = "Romário fez " + gols + 'gols';
+
+console.log(typeof frase);
+
+var ano = '2020';
+var mes =  8
+
+console.log(ano + mes); 
+
+let frase1 = 'JavaScript é "super" fácil';
+let frase2 = "JavaScript é 'super' fácil";
+let frase3 = "JavaScript é \"super\" fácil";
+let frase4 = `JavaScript é "super" fácil"`;
+// let fraseInvalida = "JavaScript é "super" fácil"; // Inválido
+
+var numero = 60;
+var farse5 = `Julio contou até ${numero} hoje!`
+
+
+console.log('Exercícios')
+// EXERCÍCIO
+// Declare uma variável contendo uma string
+let frase6 = "Olá meu nome é Ana";
+
+// Declare uma variável contendo um número dentro de uma string
+let frase7 = "Olá eu tenho " + 20 + "anos"
+
+// Declare uma variável com a sua idade
+let minhaIdade = 21;
+
+// Declare duas variáveis, uma com seu nome
+let meuNome = 'Ana Beatriz';
+// e outra com seu sobrenome e some as mesmas
+let meuSobrenome = "Ribeiro da Silva"
+let somaNomeSobrenome = `${meuNome} ${meuSobrenome}`
+
+// Coloque a seguinte frase em uma variável: It's time
+let frase8 = "It's time"
+
+// Verifique o tipo da variável que contém o seu nome
+let tipoNome = typeof meuNome;
+console.log(tipoNome)
